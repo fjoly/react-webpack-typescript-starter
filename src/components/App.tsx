9c61f283b759155bb4db6cba81166b1337564c9f
Joly <felicien.joly@gmail.com>
2022-02-01 15:22:48 +0100

    Initial commit for make some changement for stater. Add plugin MiniCss to manage css.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,15 @@
 import * as React from "react";
-import { hot } from "react-hot-loader";
-
-const reactLogo = require("./../assets/img/react_logo.svg");
-import "./../assets/scss/App.scss";
+import {hot} from "react-hot-loader";
+import "./../assets/css/App.css";
 
 class App extends React.Component<Record<string, unknown>, undefined> {
-  public render() {
-    return (
-      <div className="app">
-        <h1>Hello World!</h1>
-        <p>Foo to the barz</p>
-        <img src={reactLogo.default} height="480" />
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div>
+                <h2>Test</h2>
+            </div>
+        );
+    }
 }
 
 declare let module: Record<string, unknown>;
